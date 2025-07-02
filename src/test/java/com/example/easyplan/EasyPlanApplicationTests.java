89cd1b951e2b870e5c6f814b1b5a8450fecb923a
한민서 <hms.ideal.v@gmail.com>
2025-07-02 21:04:31 +0900

    update test yml file

--- src/test/java/com/example/easyplan/EasyPlanApplicationTests.java
+++ src/test/java/com/example/easyplan/EasyPlanApplicationTests.java
@@ -2,11 +2,16 @@ package com.example.easyplan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.easyplan.service.GeminiService;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application.yml")
 class EasyPlanApplicationTests {
+
+    @MockBean
+    private GeminiService geminiService;
 
     @Test
     void contextLoads() {
